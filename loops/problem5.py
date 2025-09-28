77edf65ddceeb11576d09da408f218c353609bbe
kishikishore2863 <kishikishore.2863@gmail.com>
2025-09-28 23:46:40 +0530

    py

--- loops/problem5.py
+++ loops/problem5.py
@@ -5,4 +5,26 @@ length = len(name)
 
 for i in range(length):
     print(name[i])
-    
+
+
+name = "kishore"
+reversed_name = name[::-1]
+print("Reversed:", reversed_name)
+
+
+name = "kishore"
+vowels = "aeiou"
+count = sum(1 for ch in name if ch.lower() in vowels)
+print("Number of vowels:", count)
+
+
+name = "kishore"
+for i, ch in enumerate(name):
+    print(f"Index {i} -> {ch}")
+
+
+name = "kishore"
+if name == name[::-1]:
+    print("Palindrome")
+else:
+    print("Not a palindrome")
